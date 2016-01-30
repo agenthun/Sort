c570fb9f74f4249bac71bb62a1f3fe2fbdc6135a
agenthun <hun333@126.com>
2016-01-31 03:52:07 +0800

    update mergeSort

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Sort
 	Exercise Sort with Java
 # List
-	SelectionSort 选择排序
-	InsertSort 插入排序
-	BubbleSort 冒泡排序
-	MergeSort 归并排序
-	QuickSort 快速排序
-	ShellSort 希尔排序
-	MinHeapSort 堆排序
+	SelectionSort 选择排序   不稳定
+	InsertSort 插入排序      稳定
+	BubbleSort 冒泡排序      稳定
+	MergeSort 归并排序       稳定
+	QuickSort 快速排序       不稳定
+	ShellSort 希尔排序       不稳定
+	MinHeapSort 堆排序       不稳定
--- src/com/agenthun/sort/MergeSort.java
+++ src/com/agenthun/sort/MergeSort.java
@@ -4,20 +4,47 @@ package com.agenthun.sort;
  * Created by Agent Henry on 2015/7/6.
  */
 public class MergeSort {
-    public static void Merge(int a[], int p, int q, int r) {
+    public static void Merge1(int a[], int left, int mid, int right) {
+        int[] temp = new int[right - left + 1];
+        int i = left;
+        int j = mid + 1;
+        int k = 0;
+
+        while (i <= mid && j <= right) {
+//            if (a[i] > a[j]) {//大->小
+            if (a[i] < a[j]) {
+                temp[k++] = a[i++];
+            } else {
+                temp[k++] = a[j++];
+            }
+        }
+
+        while (i <= mid) {
+            temp[k++] = a[i++];
+        }
+        while (j <= right) {
+            temp[k++] = a[j++];
+        }
+
+        for (int l = 0; l < temp.length; l++) {
+            a[l + left] = temp[l];
+        }
+    }
+
+    public static void Merge2(int a[], int left, int mid, int right) {
         int i, j, k, n1, n2;
-        n1 = q - p + 1;
-        n2 = r - q;
+        n1 = mid - left + 1;
+        n2 = right - mid;
         int[] L = new int[n1];
         int[] R = new int[n2];
-        for (i = 0, k = p; i < n1; i++, k++) {
+        for (i = 0, k = left; i < n1; i++, k++) {
             L[i] = a[k];
         }
-        for (i = 0, k = q + 1; i < n2; i++, k++) {
+        for (i = 0, k = mid + 1; i < n2; i++, k++) {
             R[i] = a[k];
         }
 
-        for (k = p, i = 0, j = 0; i < n1 && j < n2; k++) {
+        for (k = left, i = 0, j = 0; i < n1 && j < n2; k++) {
             if (L[i] > R[j]) {
                 a[k] = L[i];
                 i++;
@@ -34,18 +61,19 @@ public class MergeSort {
         }
     }
 
-    public static void MergeSort(int[] a, int p, int r) {
+    public static void MergeSort(int[] a, int left, int right) {
         if (a == null || a.length <= 0) return;
-        if (p < r) {
-            int q = (p + r) / 2;
-            MergeSort(a, p, q);
-            MergeSort(a, q + 1, r);
-            Merge(a, p, q, r);
+        if (left < right) {
+            int mid = (left + right) / 2;
+            MergeSort(a, left, mid);
+            MergeSort(a, mid + 1, right);
+            Merge1(a, left, mid, right);
+//            Merge2(a, left, mid, right);
         }
     }
 
     public static void main(String[] args) {
-        int[] list = {10, 5, 2, 8, 9};
+        int[] list = {10, 5, 2, 8, 2, 9};
         System.out.println("old array");
         for (int i = 0; i < list.length; i++) {
             System.out.print(list[i] + " ");
